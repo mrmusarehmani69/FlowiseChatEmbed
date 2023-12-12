302d25d7a1d3d8bdf2b7bd9cf568a49fa667a50c
mrmusarehmani69 <153620177+mrmusarehmani69@users.noreply.github.com>
2023-12-12 09:04:00 -0500

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -53,14 +53,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://www.youtube.com/watch?v=4MSxi6-RYCI'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Youtube</span>
       </a>
     </span>
   );
